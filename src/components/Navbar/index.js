c9a71a80fdd9a1a56934983a587da4c90b62ca3e
James Masaviru <jmasaviru@gmail.com>
2021-03-04 16:50:02 -0500

    Updating jumbotron & navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,10 +5,10 @@ function Navbar() {
     return (
     <div>
         <nav className="navbar navbar-expand-md justify-content-end">
-            <a className="navbar-brand" href="#aboutMe">About Me</a>
-            <a className="navbar-brand" href="#projects">Projects</a>
-            <a className="navbar-brand" href="#assignments">Assignments</a>  
-            <a className="navbar-brand" href="#contactRow">Contact</a> 
+            <a className="navbar-brand" href="#aboutMe">About Me </a>
+            <a className="navbar-brand" href="#projects"> Team Projects </a>
+            <a className="navbar-brand" href="#assignments"> Assignments </a>  
+            <a className="navbar-brand" href="#contactRow"> Connect </a> 
         </nav>
     </div>
     )
