7c2e78c9bba3d1426fb5d9f1da135a7191204e54
James Masaviru <jmasaviru@gmail.com>
2021-03-04 16:35:29 -0500

    Updating components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar';
+import Jumbotron from './components/Jumbotron';
+import Cards from './components/Cards';
+// import Divider from './components/Divider';
+import HomeworkCards from './components/hwCards';
+// import DividerTwo from './components/DividerTwo'
+import AboutMe from './components/AboutMe';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Technical from './components/Technical';
+import TwoBackgrnd from './components/TwoBG';
+import BackgroundOne from './components/BackgroundOne';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+     <Navbar/>
+     <Jumbotron/>
+     <AboutMe/>
+     <Technical/>
+     <BackgroundOne></BackgroundOne>
+     <Cards/>
+     <TwoBackgrnd></TwoBackgrnd>
+     <HomeworkCards/>
+     {/* <DividerTwo/> */}
+     <Contact/>
+     <Footer/>
+   </div>
   );
 }
 
