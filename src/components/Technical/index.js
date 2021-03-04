f6470ce3670ab056023214692f122b695de74d16
James Masaviru <jmasaviru@gmail.com>
2021-03-04 16:55:13 -0500

    Updating Tech logo

--- src/components/Technical/index.js
+++ src/components/Technical/index.js
@@ -10,7 +10,7 @@ function Technical() {
                 
                 <img className='logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1200px-CSS3_logo_and_wordmark.svg.png" alt="nice"></img>
                 <img className='logo' src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/512px-HTML5_logo_and_wordmark.svg.png' alt="nice"></img>
-                <img className='logo' src='https://malakian75s.github.io/static/media/es6.1f0e4175.svg' alt="nice"></img>
+                <img className='logo' src='https://github.githubassets.com/images/modules/logos_page/Octocat.png' alt="nice"></img>
                 <img className='logo' src='https://i0.wp.com/www.disk91.com/wp-content/uploads/2018/02/mongodb-1.png?fit=413%2C484&ssl=1' alt="nice"></img>
                 <img className='logo' src='https://miro.medium.com/max/6668/1*XP-mZOrIqX7OsFInN2ngRQ.png' alt="nice"></img>
                 <img className='logo' src='https://mildaintrainings.com/wp-content/uploads/2017/11/react-logo.png' alt="nice"></img>
