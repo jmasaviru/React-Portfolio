8053535277949183eecf55d9d2e15f893ff9b20a
James Masaviru <jmasaviru@gmail.com>
2021-03-11 16:33:47 -0500

    Updating Technologies icons

--- src/components/Technical/index.js
+++ src/components/Technical/index.js
@@ -14,7 +14,9 @@ function Technical() {
                 <img className='logo' src='https://i0.wp.com/www.disk91.com/wp-content/uploads/2018/02/mongodb-1.png?fit=413%2C484&ssl=1' alt="nice"></img>
                 <img className='logo' src='https://miro.medium.com/max/6668/1*XP-mZOrIqX7OsFInN2ngRQ.png' alt="nice"></img>
                 <img className='logo' src='https://mildaintrainings.com/wp-content/uploads/2017/11/react-logo.png' alt="nice"></img>
-                <img className='logo' src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS_XanOL-jJfrFnbIDYp9Q0e8wa4PL-Wdiicw&usqp=CAU' alt="nice"></img>           
+                <img className='logo' src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS_XanOL-jJfrFnbIDYp9Q0e8wa4PL-Wdiicw&usqp=CAU' alt="nice"></img>
+                <img className='logo' src='http://pngimg.com/uploads/mysql/mysql_PNG23.png' alt="nice"></img>
+                <img className='logo' src='https://www.pikpng.com/pngl/b/476-4763953_mobile-cloud-and-heroku-100-pure-java-heroku.png' alt="nice"></img>
 
             </div>
         </div>
