10eac186cd9339e40aebae1eb8d39b8c8c7f6348
James Masaviru <jmasaviru@gmail.com>
2021-03-11 16:27:54 -0500

    Updating 'Connect' details

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -16,6 +16,7 @@ function Contact(props){
 
     const [popoverOpen3, setPopoverOpen3] = useState(false);
 
+    /* eslint-disable no-unused-vars */
     const toggle3 = () => setPopoverOpen3(!popoverOpen3);
 
     return(
@@ -28,29 +29,29 @@ function Contact(props){
                 <div className="footer-copyright text-center py-3 ml-4">
                     <h3 className='text-left'>Connect </h3>
                     <hr></hr>
-                        <a className="btn mb-4" href="https://github.com/jmasaviru" target="_blank">
-                            <img className='btn linkImg' src={gitHubLogo}/>
+                        <a className="btn mb-4" href="https://github.com/jmasaviru" rel="noopener noreferrer" target="_blank">
+                            <img className='btn linkImg' src={gitHubLogo} alt="..."/>
                         </a>
-                        <a href="https://www.linkedin.com/in/james-masaviru-ba0a2117/" target="_blank">
-                            <img className="btn mb-4 linkImg" target="_blank" src="https://image.flaticon.com/icons/png/512/61/61109.png"></img>
+                        <a href="https://www.linkedin.com/in/james-masaviru-ba0a2117/" rel="noopener noreferrer" target="_blank">
+                            <img className="btn mb-4 linkImg" target="_blank" src="https://image.flaticon.com/icons/png/512/61/61109.png" alt="..."></img>
                         </a>
-                        <a href="https://www.youtube.com/channel/UC6hNqvr03w_KP0PwbeJzcsQ" target="_blank">
-                            <img className="btn mb-4 linkImg" target="_blank" src="https://img.flaticon.com/icons/png/512/49/49399.png"></img>
+                        <a href="https://www.youtube.com/channel/UC6hNqvr03w_KP0PwbeJzcsQ" rel="noopener noreferrer" target="_blank">
+                            <img className="btn mb-4 linkImg" target="_blank" src="https://img.flaticon.com/icons/png/512/49/49399.png" alt="..."></img>
                         </a>
-                        <a href="https://docs.google.com/document/d/1PC5i2d_d5IsvHtBSlKg0Z7JIo7KG_WIQAjI9qWX9k1c/edit?usp=sharing" target="_blank">
-                            <img className="btn mb-4 linkImg" target="_blank" src="https://static.thenounproject.com/png/143185-200.png"></img>
+                        <a href="https://docs.google.com/document/d/1PC5i2d_d5IsvHtBSlKg0Z7JIo7KG_WIQAjI9qWX9k1c/edit?usp=sharing" rel="noopener noreferrer" target="_blank">
+                            <img className="btn mb-4 linkImg" target="_blank" src="https://static.thenounproject.com/png/637927-200.png" alt="..."></img>
                         </a>
                       
-                        <a id="Popover1" type="button">
-                        <img className="btn mb-4 linkImg" target="_blank" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTwDnoITDteCOJ9-ik0MD56eyCP4CJn-oobKQzkh7pD56ti-mkvHezcYWZEHOgXRYAQddO1WQviSb2sdGa4MiHuBIQMsduneVRd_g&usqp=CAU&ec=45699845"></img>
+                        <a href="/#" id="Popover1" type="button">
+                        <img className="btn mb-4 linkImg" target="_blank" src="https://static.thenounproject.com/png/1042814-200.png" alt="..."></img>
                         </a>
                             <Popover placement="top" isOpen={popoverOpen1} target="Popover1" toggle={toggle1}>
                             <PopoverHeader>Phone Number</PopoverHeader>
                             <PopoverBody>(313) 652-4919</PopoverBody>
                         </Popover>
                         
-                        <a id="Popover2" type="button">
-                            <img className="btn mb-4 linkImg" target="_blank" src="https://image.flaticon.com/icons/png/512/8/8807.png"></img>
+                        <a href="/#" id="Popover2" type="button">
+                            <img className="btn mb-4 linkImg" target="_blank" src="https://image.flaticon.com/icons/png/512/8/8807.png" alt="..."></img>
                         </a>
                             <Popover placement="top" isOpen={popoverOpen2} target="Popover2" toggle={toggle2}>
                             <PopoverHeader>Email Address</PopoverHeader>
