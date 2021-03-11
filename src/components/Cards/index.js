9d5223a3ff5c9fc5c53344e516b90bc0200d0b40
James Masaviru <jmasaviru@gmail.com>
2021-03-11 16:23:22 -0500

    Updating Team Projects

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -1,7 +1,8 @@
 import React, { useState} from 'react';
 import './style.css';
 import { Popover, PopoverHeader, PopoverBody } from 'reactstrap';
-import datecape from '../../images/DateCapeDemo.gif';
+import datecape from '../../images/datecape.png';
+import infotech from '../../images/infotech.png';
 
 function Cards() {
     const [popoverOpen9, setPopoverOpen9] = useState(false);
@@ -9,6 +10,9 @@ function Cards() {
 
     const [popoverOpen10, setPopoverOpen10] = useState(false);
     const toggle10 = () => setPopoverOpen10(!popoverOpen10);
+
+    const [popoverOpen11, setPopoverOpen11] = useState(false);
+    const toggle11 = () => setPopoverOpen11(!popoverOpen11);
     return(
     
     <div className="container mt-2" id="projects">
@@ -16,13 +20,39 @@ function Cards() {
         <br/>   
             <h2 className="mt-1" id="teamHeading">Team Projects</h2>
             <hr></hr>
-    {/* ------------- Dish2Pass ---------- */}
+    
             <div className="row cardSection">
 
+                {/* ---------Infotech---------- */}
                 <div class="col-12 col-md-6 col-lg-4 cardDiv">
-                    <p><span class='extraWords'><b>Dish2Pass</b></span>An application that generates tasty burger, duck and pasta recipes!</p>
+                    <p><span class='extraWords'><b>Infotech University:</b></span> A University webpage that allows the user upon registering and logging-in, to gain access to different features.</p>
                         <div class="card bg-dark text-white">
-                            <img src='https://images.pexels.com/photos/2092906/pexels-photo-2092906.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' className="card-img center-text" id='dish2passPic' alt="..."/>
+                            <img src={infotech} className="card-img center-text" id='infotechPic' alt="..."/>
+                        </div>
+
+                        <br></br>
+
+                        <a href="https://any-university.herokuapp.com/home" target="_blank" rel="noopener noreferrer" className='text-center'>
+                            <button className="btn btn-dark">Live Site</button>
+                        </a>
+                        <a href="https://github.com/jmasaviru/project-three" target="_blank"rel="noopener noreferrer"className='text-center ml-1'>
+                            <button className="btn btn-dark">GitHub</button>
+                        </a>
+
+                         {/* Tech Btn */}
+                         <button className="btn btn-dark ml-1" id="Popover9">Tech</button>
+                            <Popover placement="top" isOpen={popoverOpen9} target="Popover9" toggle={toggle9}>
+                            <PopoverHeader className='text-center'>Technologies &#38; Concepts</PopoverHeader>
+                            <PopoverBody className='text-center'>Bcrypt.js, Bootstrap, Express, EsLint, Is-empty, Heroku, MongoDB, Mongoose, Node.js, Passport.js, Passport-JWT, React, React-Bootstrap &#38; Validator</PopoverBody>
+                            </Popover>
+                </div>
+            <br></br>
+
+                {/* ------------- Dish2Pass ---------- */}
+                <div class="col-12 col-md-6 col-lg-4 cardDiv">
+                    <p><span class='extraWords'><b>Dish2Pass:</b></span> An application that generates and displays different tasty burger, duck or pasta recipes!</p>
+                        <div class="card bg-dark text-white">
+                            <img src='https://images.pexels.com/photos/5688248/pexels-photo-5688248.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' className="card-img center-text" id='dish2passPic' alt="..."/>
                         </div>
 
                         <br></br>
@@ -35,16 +65,16 @@ function Cards() {
                         </a>
 
                          {/* Tech Btn */}
-                         <button className="btn btn-dark ml-1" id="Popover9">Tech</button>
-                            <Popover placement="top" isOpen={popoverOpen9} target="Popover9" toggle={toggle9}>
+                         <button className="btn btn-dark ml-1" id="Popover10">Tech</button>
+                            <Popover placement="top" isOpen={popoverOpen10} target="Popover10" toggle={toggle10}>
                             <PopoverHeader className='text-center'>Technologies &#38; Concepts</PopoverHeader>
                             <PopoverBody className='text-center'>Express, EsLint, Handlebars, Heroku, MySQL, Node, Sequelize &#38; TravisCI</PopoverBody>
                             </Popover>
                 </div>
             <br></br>
-            {/* Date Cape */}
+            {/* ----------Date Cape---------- */}
             <div class="col-12 col-md-6 col-lg-4 cardDiv">
-                    <p><span class='extraWords'><b>Date Cape</b></span> DateCape is a date night planning service app that aims to simplify date night planning..</p>
+                    <p><span class='extraWords'><b>Date Cape:</b></span> DateCape is a date night planning service app that aims to simplify date night planning while factoring in weather and location.</p>
                         <div class="card bg-dark text-white">
                             <img src={datecape} className="card-img center-text" id='datecapePic' alt="..."/>
                         </div>
@@ -54,12 +84,12 @@ function Cards() {
                         <a href="https://jmasaviru.github.io/Datecape/" target="_blank" rel="noopener noreferrer" className='text-center'>
                             <button className="btn btn-dark">Live Site</button>
                         </a>
-                        <a href="https://github.com/crackedsnowboard/project-maven" target="_blank" rel="noopener noreferrer" className='text-center ml-1'>
+                        <a href="https://github.com/jmasaviru/Datecape" target="_blank" rel="noopener noreferrer" className='text-center ml-1'>
                             <button className="btn btn-dark">GitHub</button>
                         </a>
                         {/* Tech Btn */}
-                        <button className="btn btn-dark ml-1" id="Popover10">Tech</button>
-                            <Popover placement="top" isOpen={popoverOpen10} target="Popover10" toggle={toggle10}>
+                        <button className="btn btn-dark ml-1" id="Popover11">Tech</button>
+                            <Popover placement="top" isOpen={popoverOpen11} target="Popover11" toggle={toggle11}>
                             <PopoverHeader className='text-center'>Technologies &#38; Concepts</PopoverHeader>
                             <PopoverBody className='text-center'>Bulma, Flatpickr, HTML, CSS, JavaScript &#38; jQuery</PopoverBody>
                             </Popover>
